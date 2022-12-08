8c6e120a404064313a04b0b437c38a838f7f7ed0
Logan, Timothy <tlogan@hksinc.com>
2022-12-08 11:11:04 -0600

    tweaking setup

--- setup.py
+++ setup.py
@@ -5,24 +5,15 @@ with open("README.md", "r") as fh:
 
 
 setuptools.setup(
-    name="honeybee-radiance-command-ext",
-    version="1.0.0",
-    setup_requires=['setuptools_scm'],
-    author="HKS LINE",
-    author_email="tlogan@hksinc.com",
-    description="Python wrapper around Radiance commands.",
+    name='honeybee_radiance_command_ext',
+    version='1.0.1',
+    author='HKS LINE',
+    author_email='tlogan@hksinc.com',
+    description='Extending Honeybee Radiance to include additional commands',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(exclude=["tests*"]),
-    install_requires=[],
-    classifiers=[
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent"
-    ],
-    license="AGPL-3.0"
+    url='https://github.com/logant/honeybee_radiance_command_ext',
+    license='AGPL-3.0',
+    packages=['honeybee_radiance_command_ext'],
+    install_requires=['honeybee_radiance'],
 )
